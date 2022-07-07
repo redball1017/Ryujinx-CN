4d6b3007ea5efd362c0ca79d9f2ed03a1befeb9a
redball1017 <bingyuanfn@gmail.com>
2022-07-07 22:09:36 +0800

    更新

--- git-push-pull-changes.bat
+++ git-push-pull-changes.bat
@@ -1,13 +1,3 @@
- @echo off
- title 推送Ryujinx-CN本地更新到Github和Gitee
- echo 正在添加所有文件到git暂存区
- git add .
- echo 正在提交到本地仓库
- git commit -m "更新"
- echo 正在从远程获取最新版本并merge到本地
- git pull Ryujinx-CN main
- echo 正在push到远程服务器github和Gitee
- git push -u Ryujinx-CN main
-git push -u gitee main
- pause 
- exit
+git add . && git commit -m "更新" && git pull Ryujinx-CN main && git push -u Ryujinx-CN main && git push -u gitee main
+pause 
+exit
